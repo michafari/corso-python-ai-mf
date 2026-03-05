8a27bb9eac142872ce67d1bcceb6f8401fabbfe9
Michael Farinelli <michael.fari169@gmail.com>
2026-03-05 09:33:52 +0100

    add esercizi lc

--- Netflix/netflix.py
+++ Netflix/netflix.py
@@ -1,3 +1,5 @@
+import re
+
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -7,42 +9,38 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_colwidth', None)
 dataset = pd.read_csv("NetFlix.csv")
 
-
-dataset["soup"] = dataset["director"]+" " +dataset["cast"]+" " +dataset["genres"]+ " " + dataset["description"]
+dataset["director"] = dataset["director"].fillna("")
+dataset["cast"] = dataset["cast"].fillna("")
+dataset["genres"] = dataset["genres"].fillna("")
+dataset["description"] = dataset["description"].fillna("")
+dataset["soup"] = dataset["director"]+" " +dataset["cast"]+ " " +dataset["genres"]+ " " + dataset["description"]
 new_dataset = dataset[["title","soup"]]
-new_dataset["soup"] = new_dataset["soup"].str.split()
-new_dataset["soup"] = new_dataset["soup"].astype(str).str.replace(" ", "")
-#new_dataset = new_dataset.fillna("")
-print(new_dataset.head(20))
 
-tfidf = TfidfVectorizer()
-new_dataset["matrix"] = tfidf.fit_transform(new_dataset["soup"])
+def prepare_descr(item):
+    item = item.astype(str)
+    item = item.str.replace("-", "")
+    item = item.str.replace(",", " ")
+    item = item.str.replace(".", "" )
+    return item
 
-new_dataset_final = np.hstack((new_dataset["title"], new_dataset["matrix"]))
-# Modello NearestNeighbors standard
-model_knn = NearestNeighbors(metric='cosine', algorithm='brute')
-model_knn.fit(new_dataset_final)
+new_dataset["soup"] = prepare_descr(new_dataset["soup"])
+print(new_dataset.head(50))
 
-def recommend_netflix_series(title, df, model, k=10):
+vectorizer = TfidfVectorizer(stop_words='english')
+tfidf_matrix = vectorizer.fit_transform(new_dataset["soup"])
 
-    seed = df[df["title"] == title]
+print(tfidf_matrix.shape)
+print(tfidf_matrix.nnz)
 
-    if seed.empty:
-        raise ValueError("Title doesn't exist")
+nn_model = NearestNeighbors(metric='cosine', algorithm='brute')
+nn_model.fit(tfidf_matrix)
 
-    seed_row = seed.iloc[0]
-    seed_num = seed["soup"]
-    seed_num_scaled = tfidf.transform(seed_num)
+title = "Peaky Blinders"
+row = new_dataset.iloc[new_dataset["title"] == title]
+value = row["soup"].values[0]
 
-    distances, indices = model.kneighbors(seed_num_scaled, n_neighbors=k + 1)
+input_vec = vectorizer.transform([value])
+distances, indices = nn_model.kneighbors(input_vec, n_neighbors=30)
 
-    recs = df.iloc[indices[0]].copy()
-    recs = recs[recs["title"] != title]
-
-    recs = recs.head(k)
-    recs['similarity_distance'] = distances[0][1:]
-
-    return recs[["title", "similarity_distance"]]
-
-##
-print(recommend_netflix_series("Stranger Things", new_dataset, model_knn))
+for i, idx in enumerate(indices[0]):
+    print(f"{i+1}: {new_dataset.iloc[idx]['title']} (distance: {distances[0][i]:.3f})")
